0b7c283678710be6295a97d083e902103d21c65e
eksa <eksoct@yahoo.com>
2018-08-22 19:50:53 +0800

    Add API signin

---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/users");
+
+// router.post("/signup", userController.create);
+router.post("/signin", userController.signIn);
+
+module.exports = router;
