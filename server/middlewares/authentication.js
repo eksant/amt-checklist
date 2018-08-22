c3bc7fd3b3326775989a337fc8dc5c8230f27332
eksa <eksoct@yahoo.com>
2018-08-22 19:50:08 +0800

    Update auth and author

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -1,19 +1,28 @@
-const jsonwebtoken = require('jsonwebtoken')
+const jsonwebtoken = require("jsonwebtoken");
 
-module.exports = (req,res,next) => {
+module.exports = (req, res, next) => {
   if (!req.headers.token) {
-    res.status(202).json({
-      message: 'you dont have authentication !'
-    })
+    res.status(203).json({
+      message: "you dont have authentication !"
+    });
   } else {
-    var decode = jsonwebtoken.verify(req.headers.token, process.env.JWT_KEY)
-    if (decode.name !== 'JsonWebTokenError'){
-      req.decoded = decode.admins
-      next()
+    var decode = jsonwebtoken.verify(req.headers.token, process.env.JWT_KEY);
+    // console.log('decode: ', decode)
+    if (decode.name !== "JsonWebTokenError") {
+      const { roles } = decode.user;
+      // console.log("roles", roles);
+      if (roles === "superadmin" || roles === "admin") {
+        req.decoded = decode.user[0];
+        next();
+      } else {
+        next({
+          message: "you dont have authentication !"
+        });
+      }
     } else {
       next({
-        message: 'you dont have authentication !'
-      })
+        message: "you dont have authentication !"
+      });
     }
   }
-}
+};
--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,18 +1,28 @@
-const jsonwebtoken = require('jsonwebtoken')
+const jsonwebtoken = require("jsonwebtoken");
 
-module.exports = (req,res,next)=>{
+module.exports = (req, res, next) => {
   if (!req.headers.token) {
     next({
-      message: 'you dont have authorization !'
-    })
+      message: "you dont have authorization !"
+    });
   }
-  var decode = jsonwebtoken.verify(req.headers.token, process.env.JWT_KEY)
-  if (decode.name !== 'JsonWebTokenError'){
-    req.decoded = decode.users
-    next()
+  var decode = jsonwebtoken.verify(req.headers.token, process.env.JWT_KEY);
+  // console.log("decode: ", decode);
+  if (decode.name !== "JsonWebTokenError") {
+    const { roles } = decode.user[0];
+    // console.log("roles", roles);
+    if (roles === "superadmin" || roles === "admin" || roles === "sopir" || roles === "kernet") {
+      req.decoded = decode.user[0];
+      // console.log('decode.user[0]: ', decode.user[0])
+      next();
+    } else {
+      next({
+        message: "you dont have authorization !"
+      });
+    }
   } else {
     next({
-      message: 'you dont have authorization !'
-    })
+      message: "you dont have authorization !"
+    });
   }
-}
+};
