a86905b91fd3d16d159f00c570956e42c6ac8fb3
eksa <eksoct@yahoo.com>
2018-08-26 10:36:05 +0800

    Fixed bug mongoose connection

--- server/app.js
+++ server/app.js
@@ -1,21 +1,32 @@
 require("dotenv").config();
+
+const fs = require("fs");
 const express = require("express");
-const path = require("path");
-const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const logger = require("morgan");
-const cookieParser = require("cookie-parser");
+// const redis = require("redis");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+// const cacheRedis = require("./middlewares/cache");
+// const typeDefs = fs.readFileSync("./graphql/main.graphql", "utf-8");
+// const resolverUser = require("./graphql/resolverUser");
+// const schema = makeExecutableSchema({
+//   typeDefs,
+//   resolverUser
+// });
+
 const app = express();
+// const clientRedis = redis.createClient();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-mongoose.connection.openUri("mongodb://localhost:27017/mtc");
 mongoose.Promise = global.Promise;
+mongoose.connection.openUri("mongodb://localhost:27017/mtc", {
+  autoIndex: false,
+  useNewUrlParser: true
+});
 mongoose.connection
   .once("open", () => {
     console.log("database connection success");
@@ -24,18 +35,24 @@ mongoose.connection
     console.error("database connection error", error);
   });
 
+// clientRedis.on("ready", err => {
+//   err
+//     ? console.error("redis client connection error : ", err)
+//     : console.log("redis client connection success");
+// });
+
 app.use(cors());
 app.use(logger("dev"));
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/api/", require("./routes/auth"));
+// app.use("/graphql", graphqlExpress({ schema }));
+// app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+// app.use("/api/", cacheRedis, require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
-app.use("/api/mobiltangkis", require("./routes/mobiltangkis"));
-app.use("/api/checklist", require("./routes/checklist"));
+// app.use("/api/mobiltangkis", cacheRedis, require("./routes/mobiltangkis"));
+// app.use("/api/checklist", cacheRedis, require("./routes/checklist"));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
