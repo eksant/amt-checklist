61d7cabafdd308e8347df24e20af358c420a1f03
eksa <eksoct@yahoo.com>
2018-08-26 10:41:18 +0800

    Add redis and cache middlewares

--- server/app.js
+++ server/app.js
@@ -7,11 +7,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const logger = require("morgan");
-// const redis = require("redis");
+const redis = require("redis");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 const { makeExecutableSchema } = require("graphql-tools");
 
-// const cacheRedis = require("./middlewares/cache");
+const cacheRedis = require("./middlewares/cache");
 // const typeDefs = fs.readFileSync("./graphql/main.graphql", "utf-8");
 // const resolverUser = require("./graphql/resolverUser");
 // const schema = makeExecutableSchema({
@@ -20,7 +20,7 @@ const { makeExecutableSchema } = require("graphql-tools");
 // });
 
 const app = express();
-// const clientRedis = redis.createClient();
+const clientRedis = redis.createClient();
 
 mongoose.Promise = global.Promise;
 mongoose.connection.openUri("mongodb://localhost:27017/mtc", {
@@ -35,11 +35,11 @@ mongoose.connection
     console.error("database connection error", error);
   });
 
-// clientRedis.on("ready", err => {
-//   err
-//     ? console.error("redis client connection error : ", err)
-//     : console.log("redis client connection success");
-// });
+clientRedis.on("ready", err => {
+  err
+    ? console.error("redis client connection error : ", err)
+    : console.log("redis client connection success");
+});
 
 app.use(cors());
 app.use(logger("dev"));
@@ -50,7 +50,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use("/graphql", graphqlExpress({ schema }));
 // app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 // app.use("/api/", cacheRedis, require("./routes/auth"));
-app.use("/api/users", require("./routes/users"));
+app.use("/api/users", cacheRedis, require("./routes/users"));
 // app.use("/api/mobiltangkis", cacheRedis, require("./routes/mobiltangkis"));
 // app.use("/api/checklist", cacheRedis, require("./routes/checklist"));
 
---
+++ server/middlewares/cache.js
@@ -0,0 +1,14 @@
+const redis = require("redis");
+const client = redis.createClient();
+
+module.exports = (req, res, next) => {
+  client.get("amt_cache", (err, reply) => {
+    if (err) {
+      res.send(500, err);
+    } else if (!reply) {
+      next();
+    } else {
+      res.send(JSON.parse(reply));
+    }
+  });
+};
