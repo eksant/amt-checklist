6c98c8520745bbb5b19a6a0904806db54199a2f2
eksa <eksoct@yahoo.com>
2018-09-13 01:06:34 +0800

    Add alternative endpoint google cloud

--- web-admin/src/config.js
+++ web-admin/src/config.js
@@ -5,9 +5,9 @@ const dev = {
 }
 
 const prod = {
-  EndpointRestAPI: 'http://192.168.1.7:3030/api',
-  EndpointGraphql: 'http://192.168.1.7:3030/graphql',
-  EndpointWebsocket: 'ws://192.168.1.7:3030/graphql',
+  EndpointRestAPI: 'http://192.168.1.7:3030/api' || 'http://35.197.140.110:3030/api',
+  EndpointGraphql: 'http://192.168.1.7:3030/graphql' || 'http://35.197.140.110:3030/graphql',
+  EndpointWebsocket: 'ws://192.168.1.7:3030/graphql' || 'ws://35.197.140.110:3030/graphql',
 }
 
 export default {
