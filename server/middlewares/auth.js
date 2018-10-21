539f64891550c2c11dc1ed6de4ab57bf18e1a3fc
eksant <eksoct@yahoo.com>
2018-10-22 01:02:25 +0800

    Refactor middlewares

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,42 +1,153 @@
 const _ = require('lodash')
 const jwt = require('jsonwebtoken')
-const { AuthenticationError } = require('apollo-server')
-const { ForbiddenError } = require('apollo-server')
+const { AuthenticationError, ForbiddenError } = require('apollo-server')
 const { combineResolvers, skip } = require('graphql-resolvers')
 
 // Using for Global Authentication
 const createToken = user => {
-  return jwt.sign(_.omit(user.toObject(), 'password'), process.env.JWT_KEY, {
+  const tokenUser = _.omit(user.toObject(), [
+    'password',
+    'createdBy',
+    'createdAt',
+    'updatedAt',
+    '_v',
+  ])
+  // console.log('token user', tokenUser)
+
+  return jwt.sign(tokenUser, process.env.JWT_KEY, {
     expiresIn: '8h',
   })
 }
 
 // Using for Rest API Authentication
-const validateToken = async (req, res, next) => {
+// const validateToken = async (req, res, next) => {
+//   const token = req.headers.token
+//   // console.log('token', token)
+
+//   if (token) {
+//     try {
+//       const decode = await jwt.verify(token, process.env.JWT_KEY)
+//       console.log('decode', decode)
+//       req.authUser = decode || decode.user
+//       console.log('req authUser', req.authUser)
+//       next()
+//     } catch (error) {
+//       res.status(203).json({
+//         message: 'Your session expired. Sign in again!',
+//         error,
+//       })
+//       next(error)
+//     }
+//   } else {
+//     res.status(203).json({
+//       message: 'You dont have authentication!',
+//     })
+//     next('You dont have authentication!')
+//   }
+// }
+
+const validateTokenSuperadmin = async (req, res, next) => {
   const token = req.headers.token
 
   if (token) {
     try {
       const decode = await jwt.verify(token, process.env.JWT_KEY)
-      req.authUser = decode.user
-      next()
+      if (decode.roles === 'Superadmin') {
+        req.authUser = decode
+        next()
+      } else {
+        res.status(203).json({
+          status: 203,
+          message: 'You dont have authentication!',
+        })
+        next('You dont have authentication!')
+      }
     } catch (error) {
       res.status(203).json({
+        status: 203,
         message: 'Your session expired. Sign in again!',
         error,
       })
+      next(error)
     }
   } else {
     res.status(203).json({
+      status: 203,
       message: 'You dont have authentication!',
     })
+    next('You dont have authentication!')
+  }
+}
+
+const validateTokenAdmin = async (req, res, next) => {
+  const token = req.headers.token
+
+  if (token) {
+    try {
+      const decode = await jwt.verify(token, process.env.JWT_KEY)
+      if (decode.roles === 'Superadmin' || decode.roles === 'Admin') {
+        req.authUser = decode
+        next()
+      } else {
+        res.status(203).json({
+          status: 203,
+          message: 'You dont have authentication!',
+        })
+        next('You dont have authentication!')
+      }
+    } catch (error) {
+      res.status(203).json({
+        status: 203,
+        message: 'Your session expired. Sign in again!',
+        error,
+      })
+      next(error)
+    }
+  } else {
+    res.status(203).json({
+      status: 203,
+      message: 'You dont have authentication!',
+    })
+    next('You dont have authentication!')
+  }
+}
+
+const validateTokenUser = async (req, res, next) => {
+  const token = req.headers.token
+
+  if (token) {
+    try {
+      const decode = await jwt.verify(token, process.env.JWT_KEY)
+      if (decode.roles === 'Superadmin' || decode.roles === 'Admin' || decode.roles === 'User') {
+        req.authUser = decode
+        next()
+      } else {
+        res.status(203).json({
+          status: 203,
+          message: 'You dont have authentication!',
+        })
+        next('You dont have authentication!')
+      }
+    } catch (error) {
+      res.status(203).json({
+        status: 203,
+        message: 'Your session expired. Sign in again!',
+        error,
+      })
+      next(error)
+    }
+  } else {
+    res.status(203).json({
+      status: 203,
+      message: 'You dont have authentication!',
+    })
+    next('You dont have authentication!')
   }
 }
 
 // Using for Graphql Authentication
-const verifyToken = async req => {
+const getToken = async req => {
   const token = req.headers.token
-  // console.log('token:', token)
 
   if (token) {
     try {
@@ -44,31 +155,42 @@ const verifyToken = async req => {
     } catch (error) {
       throw new AuthenticationError('Your session expired. Sign in again!')
     }
-  } else {
-    throw new AuthenticationError('You dont have authentication!')
   }
 }
 
 const isAuthenticated = (parent, args, { authUser }) =>
   authUser ? skip : new ForbiddenError('You dont have authentication!')
 
-const isSuperAdmin = combineResolvers(
+const gqlValidateTokenSuperadmin = combineResolvers(
   isAuthenticated,
   (parent, args, { authUser: { roles } }) =>
-    roles === 'Superadmin' ? skip : new ForbiddenError('You dont have authorized as superadmin!')
+    roles === 'Superadmin' ? skip : new ForbiddenError('You dont have authentication!')
 )
 
-const isAdmin = combineResolvers(
+const gqlValidateTokenAdmin = combineResolvers(
   isAuthenticated,
   (parent, args, { authUser: { roles } }) =>
-    roles === 'Admin' ? skip : new ForbiddenError('You dont have authorized as admin!')
+    roles === 'Superadmin' || roles === 'Admin'
+      ? skip
+      : new ForbiddenError('You dont have authentication!')
+)
+
+const gqlValidateTokenUser = combineResolvers(
+  isAuthenticated,
+  (parent, args, { authUser: { roles } }) =>
+    roles === 'Superadmin' || roles === 'Admin' || roles === 'User'
+      ? skip
+      : new ForbiddenError('You dont have authentication!')
 )
 
 module.exports = {
   createToken,
-  validateToken,
-  verifyToken,
-  isAuthenticated,
-  isSuperAdmin,
-  isAdmin,
+  // validateToken,
+  validateTokenSuperadmin,
+  validateTokenAdmin,
+  validateTokenUser,
+  getToken,
+  gqlValidateTokenSuperadmin,
+  gqlValidateTokenAdmin,
+  gqlValidateTokenUser,
 }
