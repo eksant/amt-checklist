71aa1759f96aaf45f5b732c8b09115a9c9b7781f
eksant <eksoct@yahoo.com>
2018-10-22 03:21:49 +0800

    Add endpoint pertamina

--- web-admin/src/config.js
+++ web-admin/src/config.js
@@ -5,11 +5,17 @@ const dev = {
 }
 
 const prod = {
-  EndpointRestAPI: 'http://192.168.1.7:3030/api' || 'http://35.197.140.110:3030/api',
-  EndpointGraphql: 'http://192.168.1.7:3030/graphql' || 'http://35.197.140.110:3030/graphql',
-  EndpointWebsocket: 'ws://192.168.1.7:3030/graphql' || 'ws://35.197.140.110:3030/graphql',
+  EndpointRestAPI: 'http://seorangeksa.com:3030/api',
+  EndpointGraphql: 'http://seorangeksa.com:3030/graphql',
+  EndpointWebsocket: 'ws://35.197.140.110:3030/graphql',
+}
+
+const pertamina = {
+  EndpointRestAPI: 'http://192.168.1.7:3030/api',
+  EndpointGraphql: 'http://192.168.1.7:3030/graphql',
+  EndpointWebsocket: 'ws://192.168.1.7:3030/graphql',
 }
 
 export default {
-  ...(process.env.REACT_APP_STAGE === 'prod' ? prod : dev), // Default to dev if not set
+  ...(process.env.REACT_APP_STAGE === 'dev' ? dev : prod ? prod : pertamina), // Default to dev if not set
 }
