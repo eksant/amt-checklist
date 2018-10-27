33c532daa6c126ab29d6356c66cb23268d7d5ffa
eksant <eksoct@yahoo.com>
2018-10-28 06:40:51 +0800

    Add config

---
+++ Amtc/src/config.js
@@ -0,0 +1,18 @@
+const dev = {
+  EndpointRestAPI: 'http://localhost:3030/api',
+  EndpointGraphql: 'http://localhost:3030/graphql',
+}
+
+const prod = {
+  EndpointRestAPI: 'http://seorangeksa.com:3030/api',
+  EndpointGraphql: 'http://seorangeksa.com:3030/graphql',
+}
+
+const pertamina = {
+  EndpointRestAPI: 'http://192.168.1.7:3030/api',
+  EndpointGraphql: 'http://192.168.1.7:3030/graphql',
+}
+
+export default {
+  ...(process.env.REACT_APP_STAGE === 'dev' ? dev : prod ? prod : pertamina), // Default to dev if not set
+}
