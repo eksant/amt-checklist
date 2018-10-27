54b2744af642f8b82b07098758496bca13e1a047
eksant <eksoct@yahoo.com>
2018-10-28 06:42:16 +0800

    Update logout

--- Amtc/src/views/Logout/index.js
+++ Amtc/src/views/Logout/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { View } from 'react-native'
 import { Container, Text } from 'native-base'
+
+import { setUserLogout } from '../../store/auth/auth.actions'
 
 class Logout extends Component {
   render() {
@@ -12,6 +16,31 @@ class Logout extends Component {
       </Container>
     )
   }
+
+  componentDidMount() {
+    this.props.setUserLogout()
+    this.props.navigation.navigate('Home')
+
+    // setTimeout(() => {
+    // }, 1000)
+  }
 }
 
-export default Logout
+// const mapStateToProps = state => {
+//   return {
+//     auth: state.auth,
+//   }
+// }
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      setUserLogout,
+    },
+    dispatch
+  )
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Logout)
