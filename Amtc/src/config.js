7812fb9983bfc7898cc295f6b17d7bd4d04f19ae
eksant <eksant@gmail.com>
2020-03-25 15:49:28 +0800

    Update config api

--- Amtc/src/config.js
+++ Amtc/src/config.js
@@ -1,17 +1,17 @@
 const dev = {
-  EndpointRestAPI: 'http://seorangeksa.com:3030/api',
-  EndpointGraphql: 'http://seorangeksa.com:3030/graphql',
+  EndpointRestAPI: 'https://apiamt.seorangeksa.com/api',
+  EndpointGraphql: 'https://apiamt.seorangeksa.com/graphql',
 }
 
-const prod = {
-  EndpointRestAPI: 'http://192.168.1.10:3030/api',
-  EndpointGraphql: 'http://192.168.1.10:3030/graphql',
-}
+// const prod = {
+//   EndpointRestAPI: 'http://192.168.1.10:3030/api',
+//   EndpointGraphql: 'http://192.168.1.10:3030/graphql',
+// }
 
-const pertamina = {
-  EndpointRestAPI: 'http://192.168.1.7:3030/api',
-  EndpointGraphql: 'http://192.168.1.7:3030/graphql',
-}
+// const pertamina = {
+//   EndpointRestAPI: 'http://192.168.1.7:3030/api',
+//   EndpointGraphql: 'http://192.168.1.7:3030/graphql',
+// }
 
 export default dev
 // export default {
