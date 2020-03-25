0009fea151c0fc840e2ccba8fa0fa02b12b6a1a7
eksant <eksant@gmail.com>
2020-03-25 15:27:24 +0800

    Update config api

--- web-admin/src/config.js
+++ web-admin/src/config.js
@@ -1,21 +1,23 @@
 const dev = {
-  EndpointRestAPI: 'http://seorangeksa.com:3030/api',
-  EndpointGraphql: 'http://seorangeksa.com:3030/graphql',
-  EndpointWebsocket: 'ws://35.197.140.110:3030/graphql',
+  EndpointRestAPI: 'https://apiamt.seorangeksa.com/api',
+  EndpointGraphql: 'https://apiamt.seorangeksa.com/graphql',
+  // EndpointWebsocket: 'ws://35.197.140.110:3030/graphql',
 }
 
-const prod = {
-  EndpointRestAPI: 'http://localhost:3030/api',
-  EndpointGraphql: 'http://localhost:3030/graphql',
-  EndpointWebsocket: 'ws://localhost:3030/graphql',
-}
+// const prod = {
+//   EndpointRestAPI: 'http://localhost:3030/api',
+//   EndpointGraphql: 'http://localhost:3030/graphql',
+//   EndpointWebsocket: 'ws://localhost:3030/graphql',
+// }
 
-const pertamina = {
-  EndpointRestAPI: 'http://192.168.1.7:3030/api',
-  EndpointGraphql: 'http://192.168.1.7:3030/graphql',
-  EndpointWebsocket: 'ws://192.168.1.7:3030/graphql',
-}
+// const pertamina = {
+//   EndpointRestAPI: 'http://192.168.1.7:3030/api',
+//   EndpointGraphql: 'http://192.168.1.7:3030/graphql',
+//   EndpointWebsocket: 'ws://192.168.1.7:3030/graphql',
+// }
 
-export default {
-  ...(process.env.REACT_APP_STAGE === 'dev' ? dev : prod ? prod : pertamina), // Default to dev if not set
-}
+export default dev
+
+// export default {
+//   ...(process.env.REACT_APP_STAGE === 'dev' ? dev : prod ? prod : pertamina), // Default to dev if not set
+// }
